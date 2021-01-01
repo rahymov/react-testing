04b558fb089a43b792c22e2e0ed4f0643c672f34
Yazgylych Rahymov <y.rahymov@gmail.com>
2021-01-01 14:22:11 -0500

    first commit and test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div);
+  expect(div.innerHTML).toContain('Hi sup!');
+  // const linkElement = screen.getByText('Hi sup!');
+  // expect(linkElement).toBeInTheDocument();
 });
