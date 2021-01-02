82f07fdd327b706ed848b9aa3db6048aedcb0bf5
Yazgylych Rahymov <y.rahymov@gmail.com>
2021-01-01 21:05:39 -0500

    Add redux provider, store and comment box reducer

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -17,3 +17,24 @@ it('has a textarea and button', () => {
     expect(wrapped.find('textarea').length).toEqual(1)
     expect(wrapped.find('button').length).toEqual(1)
 })
+
+describe('the textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  })
+  it('has a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+  })
+
+  it('when form submitted empty textarea', () => {
+    // if we did not test above that textarea is equal to output we needed to add it
+    // expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
+
+})
